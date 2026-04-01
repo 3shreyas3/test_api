7b4cfcd799e338cdd8d3adb10e46bafbbc47f811
3shreyas3 <rvshreyas131@gmail.com>
2026-04-01 10:03:02 +0530

    Add Flask routes for home, posts, and comments

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('album.html')
+
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
+
+
+@app.route('/comments')
+def comments():
+    return render_template('comments.html')
+if __name__ == '__main__':
+    app.run(debug=True)
