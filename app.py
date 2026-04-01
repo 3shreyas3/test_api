73d9af3eea67b6149325602b06a6bf66c7f30ca2
3shreyas3 <rvshreyas131@gmail.com>
2026-04-01 11:13:33 +0530

    Add API endpoints for posts, comments, and albums

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('album.html')
-
 
 @app.route('/posts')
-def posts():
-    return render_template('posts.html')
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    payload = {}
+    headers = {}
 
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "successful message",
+        "status_code": 200
+    })
 
 @app.route('/comments')
-def comments():
-    return render_template('comments.html')
+def get_comments():
+    url = "https://jsonplaceholder.typicode.com/comments"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
+    })
+
+@app.route('/albums')
+def get_albums():
+    url = "https://jsonplaceholder.typicode.com/albums"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "successful message",
+        "status_code": 200
+    })
+
 if __name__ == '__main__':
     app.run(debug=True)
